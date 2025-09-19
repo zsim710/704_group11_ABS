af942ef6bc76694bf49bade73f56e7ab5e51e918
Zeno Simunic <zsim710@aucklanduni.ac.nz>
2025-09-19 20:50:05 +1200

    added a signal for posOrder?

--- COMPSYS704_Assignment1/src/run/GUI.java
+++ COMPSYS704_Assignment1/src/run/GUI.java
@@ -3,6 +3,6 @@ package run;
 public class GUI implements java.lang.Runnable {
 	public void run() {
 		// org.compsys704.CapLoader.main(null);
-		org.compsys704.EABS_GUI.main(null);
+		org.compsys704.POS.main(null);
 	}
 }
